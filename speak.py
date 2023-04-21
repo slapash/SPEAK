e12b9d3bfd2c540ee49934c303b2c496e565c38c
Omar Benaidy <benaidy.omar@gmail.Com>
2023-04-21 15:18:47 +0000

    Add files via upload

---
+++ speak.py
@@ -0,0 +1,32 @@
+import PySimpleGUI as sg
+from gtts import gTTS
+import os
+
+def text_to_speech(text, language='en'):
+    tts = gTTS(text=text, lang=language)
+    tts.save("output.mp3")
+    os.system("mpg123 output.mp3")
+
+sg.theme('DarkBlue')
+
+layout = [
+    [sg.Text('écrivez votre texte:')],
+    [sg.Multiline('', size=(60, 10), key='input_text')],
+    [sg.Button('lire'), sg.Button('enregistrer le texte'), sg.Button('Exit')]
+]
+
+window = sg.Window('sauvegarde de texte et TTS', layout)
+
+while True:
+    event, values = window.read()
+    
+    if event in (None, 'Exit'):
+        break
+    elif event == 'lire':
+        text_to_speech(values['input_text'])
+    elif event == 'enregistrer le texte':
+        with open('output.txt', 'w') as f:
+            f.write(values['input_text'])
+        sg.popup('Text enregistré')
+
+window.close()
